2e99199c952fa0e19fd5c1fdf5ce3d7469ce348d
mmattbtw <matt@mmatt.net>
2024-02-23 18:50:42 -0600

    fix regex to only include link

--- src/index.ts
+++ src/index.ts
@@ -59,10 +59,16 @@ client.once(Events.ClientReady, (readyClient) => {
 });
 
 client.on(Events.MessageCreate, async (message) => {
-  console.log(message.content);
   if (message.content.includes("https://music.apple.com")) {
     // find the first instance of the link
-    const link = message.content.match(/https:\/\/music.apple.com\/.*/);
+    // the link can be in the middle of the message, so edit the regex to only have include the link without the rest of the message
+    // const link = message.content.match(/https:\/\/music.apple.com\/.*/);
+
+    const link = message.content.match(
+      /https?:\/\/(?:itunes\.apple\.com\/|music\.apple\.com\/)(?:[^\/]+\/)?(?:album|artist|song|album\/[^\/]+\/\w+)(?:\/[^\/]+)?\/\d+(?:\?[^\/\s]*)?/
+    );
+    console.log(link);
+
     if (link) {
       const apiRequest = await fetch(
         `https://api.song.link/v1-alpha.1/links?url=${link}&userCountry=US&songIfSingle=true`
